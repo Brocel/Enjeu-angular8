460dacef986435bf592d817faa80bd4e9f90e896
Brocel <geenens.prof@gmail.com>
2019-08-22 16:22:41 +0200

    Edit joueurs.component: définition d'un joueur

--- src/app/classes/joueur.ts
+++ src/app/classes/joueur.ts
@@ -1,7 +1,8 @@
+import { Personnage } from 'personnage';
 export class Joueur {
   id: number;
   email: email;
   name: string;
   pword: string;
-  
+
 }
--- src/app/joueurs/joueurs.component.ts
+++ src/app/joueurs/joueurs.component.ts
@@ -1,16 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Joueur } from '../classes/joueur';
 
 @Component({
-  selector: 'app-joueurs',
-  templateUrl: './joueurs.component.html',
-  styleUrls: ['./joueurs.component.css']
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
 })
-export class JoueursComponent implements OnInit {
+export class HeroesComponent implements OnInit {
+  joueur: Joueur = {
+    id: 1,
+    name: 'Brocel',
+    email: 'geenens.prof@gmail.com',
+    pword: 'C1secret'
+  };
 
   constructor() { }
 
   ngOnInit() {
   }
-  joueur = 'Brocel';
 
 }
